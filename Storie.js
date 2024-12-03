efef6aa117d1d1dc438e7de58fed330d3417bb06
Pablo H. <62629280+pabloshew@users.noreply.github.com>
2024-12-03 13:42:56 -0300

    Add files via upload

---
+++ Storie.js
@@ -0,0 +1,57 @@
+var crl1 = document.getElementById('myCrl1').getContext('2d');
+crl1.beginPath();
+crl1.arc(60,100,40,0,2 * Math.PI);
+crl1.fillStyle= "#6604BB";
+crl1.fill();
+crl1.linewidth= 5;
+crl1.strokeStyle= "#dbdbdb";
+crl1.stroke();
+
+
+var crl2 = document.getElementById('myCrl2').getContext('2d');
+crl2.beginPath();
+crl2.arc(60,100,40,0,2 * Math.PI);
+crl2.fillStyle= "#198309";
+crl2.fill();
+crl2.linewidth= 5;
+crl2.strokeStyle= "#dbdbdb";
+crl2.stroke();
+
+
+var crl3 = document.getElementById('myCrl3').getContext('2d');
+crl3.beginPath();
+crl3.arc(60,100,40,0,2 * Math.PI);
+crl3.fillStyle= "#9B0101";
+crl3.fill();
+crl3.linewidth= 5;
+crl3.strokeStyle= "#dbdbdb";
+crl3.stroke();
+
+var crl4 = document.getElementById('myCrl4').getContext('2d');
+crl4.beginPath();
+crl4.arc(60,100,40,0,2 * Math.PI);
+crl4.fillStyle= "#4169E1";
+crl4.fill();
+crl4.linewidth= 5;
+crl4.strokeStyle= "#dbdbdb";
+crl4.stroke();
+
+
+var crl5 = document.getElementById('myCrl5').getContext('2d');
+crl5.beginPath();
+crl5.arc(60,100,40,0,2 * Math.PI);
+crl5.fillStyle= "#0DF1D7";
+crl5.fill();
+crl5.linewidth= 5;
+crl5.strokeStyle= "#dbdbdb";
+crl5.stroke();
+
+var crl6 = document.getElementById('myCrl6').getContext('2d');
+crl6.beginPath();
+crl6.arc(60,100,40,0,2 * Math.PI);
+crl6.fillStyle= "#060270";
+crl6.fill();
+crl6.linewidth= 5;
+crl6.strokeStyle= "#dbdbdb";
+crl6.stroke();
+
